5adc91b57a98809116902c0d5d65ce19015a9c6f
nishankRadicleInc <nsinghal@radicleinc.com>
2021-03-20 14:17:08 +0530

    first commit

---
+++ src/main/java/com/example/bookstore/repository/BooksRepository.java
@@ -0,0 +1,17 @@
+package com.example.bookstore.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.bookstore.domain.Books;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Books, Integer>{
+
+	@Query("SELECT obj FROM Livro obj WHERE obj.categoria.id = :id_cat ORDER BY titulo")
+	List<Books> findAllByCategoria(@Param(value = "id_cat") Integer id_cat);
+
+}
